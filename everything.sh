6a3930e6dd8b62145462898bad81511df277ea42
hashit-project <58629392+hashit-project@users.noreply.github.com>
2019-12-07 17:03:10 +0000

    Create everything.sh

---
+++ everything.sh
@@ -0,0 +1,15 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update
+sudo apt-get install aptitude -y
+sudo aptitude install -y build-essential g++-8 gcc-8 git libboost-all-dev python-pip libssl-dev screen
+sudo pip install cmake
+export CC=gcc-8
+export CXX=g++-8
+git clone -b master --single-branch https://github.com/hashit-project/hashit
+cd derogold
+mkdir build
+cd build
+cmake ..
+make
+cd src
+screen ./hashitd
