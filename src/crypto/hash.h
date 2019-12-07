c61bee39b37b4c05e14b6bcebb46567f91d715ec
hashit-project <58629392+hashit-project@users.noreply.github.com>
2019-12-07 16:58:10 +0000

    Update hash.h

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -25,9 +25,9 @@
 #define CN_DARK_SCRATCHPAD 524288
 #define CN_DARK_ITERATIONS 262144
 // replace CN TRTL with CN HT as we wont need to go back 
-#define CN_TURTLE_PAGE_SIZE 262144
-#define CN_TURTLE_SCRATCHPAD 262144
-#define CN_TURTLE_ITERATIONS 131072
+#define CN_TURTLE_PAGE_SIZE 131072
+#define CN_TURTLE_SCRATCHPAD 65536
+#define CN_TURTLE_ITERATIONS 32768
 
 // CryptoNight Soft Shell Definitions
 #define CN_SOFT_SHELL_MEMORY 262144 // This defines the lowest memory utilization for our curve
